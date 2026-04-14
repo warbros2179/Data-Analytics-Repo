44b64157121c5d7492564b6f3f6119cebc7fcdec
david ocampo <docampo@my.yearupunited.org>
2026-04-14 10:30:48 -0500

    Added queries of actor and film

---
+++ Ex1B_first_query.sql
@@ -0,0 +1,8 @@
+/*	
+Sakila is a database used to collect information about people and their personal information
+*/
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
